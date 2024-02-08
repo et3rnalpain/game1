801970eedfaf4c9b39d0b9ce3ea2b97eb45a214e
et3rnalpain <gjobovi4@mail.ru>
2024-02-08 13:57:21 +0300

    SFML 2 6 0

--- game1.cpp
+++ game1.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
     std::cout << "pasha BOBS!\n";
-    
+    RenderWindow window(VideoMode(1200, 800), "IGRA");
+    return 0;
 }
